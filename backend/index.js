d6323cc8b50e2612874451b34a84c5d94f011c0b
Saritoz <nddung1119@gmail.com>
2023-05-27 21:29:43 +0700

    change cors config

--- backend/index.js
+++ backend/index.js
@@ -15,12 +15,7 @@ const PORT = 7777;
 
 mongoose.connect(process.env.DB_URL).then(() => console.log("connected to DB"));
 
-app.use(
-  cors({
-    origin: process.env.ALLOW_CORS_URL,
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(cookieparser());
 app.use(express.json());
 
